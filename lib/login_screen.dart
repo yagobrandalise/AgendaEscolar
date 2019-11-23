8d14a43a7960cda40f76875a09d8259c089e9506
douglasslaviero <emaildodouglasslaviero@gmail.com>
2019-11-23 13:02:29 -0300

    Ajustado tela de login e cadastro

--- lib/login_screen.dart
+++ lib/login_screen.dart
@@ -2,7 +2,14 @@ import 'package:flutter/material.dart';
 import 'agenda_screen.dart';
 
 class LoginScreen extends StatelessWidget {
+  final _emailController = TextEditingController();
+  final _passwordController = TextEditingController();
+
   void login(BuildContext ctx) {
+    if (_emailController.text.isEmpty || _passwordController.text.isEmpty) {
+      return;
+    }
+
     Navigator.of(ctx).push(
       MaterialPageRoute(
         builder: (_) => AgendaScreen(),
@@ -14,27 +21,47 @@ class LoginScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Login'),
+        title: const Text(
+          'Login',
+        ),
       ),
-      body: Column(
-        children: <Widget>[
-          TextField(
-            decoration: InputDecoration(
-              labelText: 'Email',
-            ),
-            keyboardType: TextInputType.emailAddress,
+      body: Center(
+        child: Container(
+          height: 200,
+          padding: EdgeInsets.symmetric(horizontal: 20),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: <Widget>[
+              TextField(
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(),
+                  labelText: 'Email',
+                ),
+                controller: _emailController,
+                keyboardType: TextInputType.emailAddress,
+              ),
+              TextField(
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(),
+                  labelText: 'Senha',
+                ),
+                controller: _passwordController,
+                obscureText: true,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.end,
+                children: <Widget>[
+                  RaisedButton(
+                    child: Text('Logar'),
+                    color: Theme.of(context).primaryColor,
+                    textColor: Theme.of(context).textTheme.button.color,
+                    onPressed: () => login(context),
+                  ),
+                ],
+              )
+            ],
           ),
-          TextField(
-            decoration: InputDecoration(
-              labelText: 'Senha',
-            ),
-            obscureText: true,
-          ),
-          RaisedButton(
-            child: Text('Logar'),
-            onPressed: () => login(context),
-          )
-        ],
+        ),
       ),
     );
   }
--- lib/register_screen.dart
+++ lib/register_screen.dart
@@ -2,16 +2,34 @@ import 'package:flutter/material.dart';
 import 'agenda_screen.dart';
 
 class RegisterScreen extends StatelessWidget {
- final snackbar = SnackBar(content: Text('Cadastrado com sucesso!'));
+  final _nameController = TextEditingController();
+  final _emailController = TextEditingController();
+  final _passwordController = TextEditingController();
 
-  void register(BuildContext ctx) {
-    // Scaffold.of(ctx).showSnackBar(snackbar);
+  void _register(BuildContext ctx) {
     Navigator.of(ctx).push(
       MaterialPageRoute(
         builder: (_) => AgendaScreen(),
       ),
     );
   }
+  
+  // void _presentDatePicker() {
+  //   showDatePicker(
+  //     context: context,
+  //     initialDate: DateTime.now(),
+  //     firstDate: DateTime(2019),
+  //     lastDate: DateTime.now(),
+  //   ).then((pickedDate) {
+  //     if (pickedDate == null) {
+  //       return;
+  //     }
+
+  //     setState(() {
+  //       _selectedDate = pickedDate;
+  //     });
+  //   });
+  // }
 
   @override
   Widget build(BuildContext context) {
@@ -21,52 +39,71 @@ class RegisterScreen extends StatelessWidget {
           'Cadastro',
         ),
       ),
-      body: Column(
-        children: <Widget>[
-          TextField(
-            decoration: InputDecoration(
-              labelText: 'Nome',
-            ),
+      body: Center(
+        child: Container(
+          padding: EdgeInsets.all(20),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: <Widget>[
+              TextField(
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(),
+                  labelText: 'Nome',
+                ),
+              ),
+              TextField(
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(),
+                  labelText: 'Data de nascimento',
+                ),
+                keyboardType: TextInputType.numberWithOptions(
+                  decimal: false,
+                ),
+              ),
+              TextField(
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(),
+                  labelText: 'Cpf',
+                ),
+                keyboardType: TextInputType.numberWithOptions(
+                  decimal: false,
+                ),
+              ),
+              TextField(
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(),
+                  labelText: 'Email',
+                ),
+                keyboardType: TextInputType.emailAddress,
+              ),
+              TextField(
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(),
+                  labelText: 'Senha',
+                ),
+                obscureText: true,
+              ),
+              TextField(
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(),
+                  labelText: 'Confirmar senha',
+                ),
+                obscureText: true,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.end,
+                children: <Widget>[
+                  RaisedButton(
+                    child: Text('Cadastrar'),
+                    color: Theme.of(context).primaryColor,
+                    textColor: Theme.of(context).textTheme.button.color,
+                    onPressed: () => _register(context),
+                  ),
+                ],
+              )
+            ],
           ),
-          TextField(
-            decoration: InputDecoration(
-              labelText: 'Idade',
-            ),
-            keyboardType: TextInputType.numberWithOptions(
-              decimal: false,
-            ),
-          ),
-          TextField(
-            decoration: InputDecoration(
-              labelText: 'Cpf',
-            ),
-            keyboardType: TextInputType.numberWithOptions(
-              decimal: false,
-            ),
-          ),
-          TextField(
-            decoration: InputDecoration(
-              labelText: 'Email',
-            ),
-            keyboardType: TextInputType.emailAddress,
-          ),
-          TextField(
-            decoration: InputDecoration(
-              labelText: 'Senha',
-            ),
-            obscureText: true,
-          ),
-          TextField(
-            decoration: InputDecoration(
-              labelText: 'Confirmar senha',
-            ),
-            obscureText: true,
-          ),
-          RaisedButton(
-            child: Text('Cadastrar'),
-            onPressed: () => register(context),
-          )
-        ],
+        ),
       ),
     );
   }
