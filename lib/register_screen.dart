b59385fdd0ec1fea7c6a7c6f62dd8edbdea52728
douglasslaviero <emaildodouglasslaviero@gmail.com>
2019-11-24 13:24:45 -0300

    Adicionado calendario para selecionar data

--- lib/register_screen.dart
+++ lib/register_screen.dart
@@ -1,35 +1,52 @@
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 import 'agenda_screen.dart';
 
 class RegisterScreen extends StatelessWidget {
   final _nameController = TextEditingController();
+  final _birthdayController = TextEditingController();
+  final _cpfController = TextEditingController();
   final _emailController = TextEditingController();
   final _passwordController = TextEditingController();
+  final _passwordController2 = TextEditingController();
+  DateTime _selectedDate;
 
   void _register(BuildContext ctx) {
+    if (_nameController.text.isEmpty ||
+        _birthdayController.text.isEmpty ||
+        _cpfController.text.isEmpty ||
+        _emailController.text.isEmpty ||
+        _passwordController.text.isEmpty ||
+        _passwordController2.text.isEmpty) {
+      return;
+    }
+
+    if (_passwordController.text != _passwordController2.text) {
+      return;
+    }
+
     Navigator.of(ctx).push(
       MaterialPageRoute(
         builder: (_) => AgendaScreen(),
       ),
     );
   }
-  
-  // void _presentDatePicker() {
-  //   showDatePicker(
-  //     context: context,
-  //     initialDate: DateTime.now(),
-  //     firstDate: DateTime(2019),
-  //     lastDate: DateTime.now(),
-  //   ).then((pickedDate) {
-  //     if (pickedDate == null) {
-  //       return;
-  //     }
 
-  //     setState(() {
-  //       _selectedDate = pickedDate;
-  //     });
-  //   });
-  // }
+  void _presentDatePicker(BuildContext ctx) {
+    showDatePicker(
+      context: ctx,
+      initialDate: DateTime.now(),
+      firstDate: DateTime(2019),
+      lastDate: DateTime.now(),
+    ).then((pickedDate) {
+      if (pickedDate == null) {
+        return;
+      }
+
+      _birthdayController.text = DateFormat.yMd().format(pickedDate);
+      _selectedDate = pickedDate;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -50,21 +67,38 @@ class RegisterScreen extends StatelessWidget {
                   border: OutlineInputBorder(),
                   labelText: 'Nome',
                 ),
+                controller: _nameController,
               ),
-              TextField(
-                decoration: InputDecoration(
-                  border: OutlineInputBorder(),
-                  labelText: 'Data de nascimento',
-                ),
-                keyboardType: TextInputType.numberWithOptions(
-                  decimal: false,
-                ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: <Widget>[
+                  Expanded(
+                    child: TextField(
+                      decoration: InputDecoration(
+                        border: OutlineInputBorder(),
+                        labelText: 'Data de nascimento',
+                      ),
+                      keyboardType: TextInputType.datetime,
+                      controller: _birthdayController,
+                      enabled: false,
+                    ),
+                  ),
+                  FlatButton(
+                    textColor: Theme.of(context).primaryColor,
+                    child: Icon(
+                      Icons.calendar_today,
+                      size: 40,
+                    ),
+                    onPressed: () => _presentDatePicker(context),
+                  ),
+                ],
               ),
               TextField(
                 decoration: InputDecoration(
                   border: OutlineInputBorder(),
                   labelText: 'Cpf',
                 ),
+                controller: _cpfController,
                 keyboardType: TextInputType.numberWithOptions(
                   decimal: false,
                 ),
@@ -74,6 +108,7 @@ class RegisterScreen extends StatelessWidget {
                   border: OutlineInputBorder(),
                   labelText: 'Email',
                 ),
+                controller: _emailController,
                 keyboardType: TextInputType.emailAddress,
               ),
               TextField(
@@ -81,6 +116,7 @@ class RegisterScreen extends StatelessWidget {
                   border: OutlineInputBorder(),
                   labelText: 'Senha',
                 ),
+                controller: _passwordController,
                 obscureText: true,
               ),
               TextField(
@@ -88,6 +124,7 @@ class RegisterScreen extends StatelessWidget {
                   border: OutlineInputBorder(),
                   labelText: 'Confirmar senha',
                 ),
+                controller: _passwordController2,
                 obscureText: true,
               ),
               Row(
